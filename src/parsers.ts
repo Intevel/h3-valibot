2eeaca9caa6d5f87d8fcada6c9ceb618d9b47bcb
Kilisei <seitzkili+Github@gmail.com>
2024-10-17 23:12:50 +0200

    fix: update parsers

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,14 +1,9 @@
-import {
-  createError,
-  getRouterParams,
-  readBody,
-  type H3Event,
-  getQuery,
-} from "h3";
-import { type BaseSchemaAsync, parseAsync, Output } from "valibot";
+import type { BaseIssue, BaseSchemaAsync } from 'valibot'
+import { createError, getQuery, getRouterParams, type H3Event, readBody } from 'h3'
+import { parseAsync } from 'valibot'
 
-const DEFAULT_ERROR_MESSAGE = "Bad Request";
-const DEFAULT_ERROR_STATUS = 400;
+const DEFAULT_ERROR_MESSAGE = 'Bad Request'
+const DEFAULT_ERROR_STATUS = 400
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function createBadRequest(error: any) {
@@ -16,24 +11,25 @@ function createBadRequest(error: any) {
     statusCode: DEFAULT_ERROR_STATUS,
     statusText: DEFAULT_ERROR_MESSAGE,
     data: error,
-  });
+  })
 }
 
 /**
  * Parse and validate request query from event handler. Throws an error if validation fails.
- * @param event
- * @param schema
+ * @param event - A H3 event object.
+ * @param schema - A Valibot Schema
  */
-export async function useValidatedQuery<T extends BaseSchemaAsync>(
+export async function useValidatedQuery<TInput, TOutput, TIssue extends BaseIssue<unknown>>(
   event: H3Event,
-  schema: T
-): Promise<Output<T>> {
+  schema: BaseSchemaAsync<TInput, TOutput, TIssue>,
+): Promise<TOutput> {
   try {
-    const query = getQuery(event);
-    const parsed = await parseAsync<T>(schema, query);
-    return parsed;
-  } catch (error) {
-    throw createBadRequest(error);
+    const query = getQuery(event)
+    const parsed = await parseAsync(schema, query)
+    return parsed
+  }
+  catch (error) {
+    throw createBadRequest(error)
   }
 }
 
@@ -42,33 +38,35 @@ export async function useValidatedQuery<T extends BaseSchemaAsync>(
  * @param event - A H3 event object.
  * @param schema - A Valibot Schema
  */
-export async function useValidatedBody<T extends BaseSchemaAsync>(
+export async function useValidatedBody<TInput, TOutput, TIssue extends BaseIssue<unknown>>(
   event: H3Event,
-  schema: T
-): Promise<Output<T>> {
+  schema: BaseSchemaAsync<TInput, TOutput, TIssue>,
+): Promise<TOutput> {
   try {
-    const body = await readBody(event);
-    const parsed = await parseAsync<T>(schema, body);
-    return parsed;
-  } catch (error) {
-    throw createBadRequest(error);
+    const body = await readBody(event)
+    const parsed = await parseAsync(schema, body)
+    return parsed
+  }
+  catch (error) {
+    throw createBadRequest(error)
   }
 }
 
 /**
- * Parse and validate request body from event handler. Throws an error if validation fails.
+ * Parse and validate request params from event handler. Throws an error if validation fails.
  * @param event - A H3 event object.
  * @param schema - A Valibot Schema
  */
-export async function useValidatedParams<T extends BaseSchemaAsync>(
+export async function useValidatedParams<TInput, TOutput, TIssue extends BaseIssue<unknown>>(
   event: H3Event,
-  schema: T
-): Promise<Output<T>> {
+  schema: BaseSchemaAsync<TInput, TOutput, TIssue>,
+): Promise<TOutput> {
   try {
-    const params = getRouterParams(event);
-    const parsed = await parseAsync<T>(schema, params);
-    return parsed;
-  } catch (error) {
-    throw createBadRequest(error);
+    const params = getRouterParams(event)
+    const parsed = await parseAsync(schema, params)
+    return parsed
+  }
+  catch (error) {
+    throw createBadRequest(error)
   }
 }
