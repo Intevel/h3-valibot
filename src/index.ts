b93370ac31d814bedaa71a24f5024b7e76cd722e
Sandros94 <sandro.circi@digitoolmedia.com>
2024-11-12 19:51:22 +0100

    up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-export { useValidatedBody, useValidatedParams, useValidatedQuery } from './parsers';
+import {
+  useSafeValidatedBody,
+  useSafeValidatedParams,
+  useSafeValidatedQuery,
+  useValidatedBody,
+  useValidatedParams,
+  useValidatedQuery,
+} from './parsers'
+
+import {
+  boolAsString,
+  checkboxAsString,
+  intAsString,
+  numAsString,
+} from './schemas'
+
+export const vh = {
+  useSafeValidatedBody,
+  useSafeValidatedParams,
+  useSafeValidatedQuery,
+  useValidatedBody,
+  useValidatedParams,
+  useValidatedQuery,
+  boolAsString,
+  checkboxAsString,
+  intAsString,
+  numAsString,
+}
+
+export {
+  useSafeValidatedBody,
+  useSafeValidatedParams,
+  useSafeValidatedQuery,
+  useValidatedBody,
+  useValidatedParams,
+  useValidatedQuery,
+} from './parsers'
+
+export * as v from 'valibot'
