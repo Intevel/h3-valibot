634f8131b3d4f865cfb79ea7f9be719e37cf3ddb
Sandros94 <sandro.circi@digitoolmedia.com>
2024-11-12 20:26:29 +0100

    feat: body validation vitest

---
+++ test/body.test.ts
@@ -0,0 +1,49 @@
+import type { App } from 'h3'
+import type { Test } from 'supertest'
+import type TestAgent from 'supertest/lib/agent'
+import { createApp, eventHandler, toNodeListener } from 'h3'
+import supertest from 'supertest'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { useSafeValidatedBody, useValidatedBody, v } from '../src'
+
+describe('useValidatedBody', () => {
+  let app: App
+  let request: TestAgent<Test>
+
+  beforeEach(() => {
+    app = createApp({ debug: false })
+    request = supertest(toNodeListener(app))
+  })
+
+  const bodySchema = v.object({
+    optional: v.optional(v.string()),
+    required: v.boolean(),
+  })
+
+  it('returns 200 OK if body matches validation schema', async () => {
+    app.use('/validate', eventHandler(event => useValidatedBody(event, bodySchema)))
+
+    const res = await request.post('/validate').send({ required: true })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchSnapshot()
+  })
+
+  it('throws 400 Bad Request if body does not match validation schema', async () => {
+    app.use('/validate', eventHandler(event => useValidatedBody(event, bodySchema)))
+
+    const res = await request.post('/validate').send({})
+
+    expect(res.status).toEqual(400)
+    expect(res.body).toMatchSnapshot()
+  })
+
+  it('doesn\'t throw 400 Bad Request if body does not match validation schema', async () => {
+    app.use('/validate', eventHandler(event => useSafeValidatedBody(event, bodySchema)))
+
+    const res = await request.post('/validate').send({})
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchSnapshot()
+  })
+})
