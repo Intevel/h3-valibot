1bfa47b08a94c7202ce1c3966608273c52822c45
Sandros94 <sandro.circi@digitoolmedia.com>
2024-11-12 19:54:04 +0100

    chore: update playground

--- playground/app.ts
+++ playground/app.ts
@@ -1,26 +1,25 @@
-import { createApp, createRouter, eventHandler } from "h3";
-import { email, minLength, string, objectAsync } from 'valibot';
+import { createApp, createRouter, eventHandler } from 'h3'
+import * as v from 'valibot'
 import { useValidatedBody } from '../src/parsers'
 
-export const app = createApp();
+export const app = createApp()
 
-const router = createRouter();
-app.use(router);
+const router = createRouter()
+app.use(router)
 
 router.get(
-  "/",
+  '/',
   eventHandler((event) => {
-    return { path: event.path, message: "Hello World!" };
+    return { path: event.path, message: 'Hello World!' }
   }),
-);
+)
 
-router.post("/echo", eventHandler(async (event) => {
-    const LoginSchema = objectAsync({
-        email: string([email()]),
-        password: string([minLength(8)]),
-    });
+router.post('/echo', eventHandler(async (event) => {
+  const LoginSchema = v.object({
+    email: v.pipe(v.string(), v.email()),
+    password: v.pipe(v.string(), v.minLength(8)),
+  })
 
-    const body = await useValidatedBody(event, LoginSchema);
-    return body;
-  }),
-);
+  const body = await useValidatedBody(event, LoginSchema)
+  return body
+}))
